c2a90cf0c12f825bf7f01ca9745fe1701d764ed4
John Cartwright <80010510+mrjc3@users.noreply.github.com>
2021-09-01 15:19:03 -0700

    Update alphavantage_api.py

--- alphavantage_api.py
+++ alphavantage_api.py
@@ -4,14 +4,14 @@ from twilio.rest import Client
 
 
 alphavantage_url = "https://www.alphavantage.co/query"
-ALPHAVANTAGE_API_KEY = "C5N1XKZX63E8KBUX"
+ALPHAVANTAGE_API_KEY = 
 
 news_url = "https://newsapi.org/v2/everything"
 # news_api_key = os.getenv('NEWS_API_KEY')
-news_api_key = "adb9dc224790479b9170f97f22e1d0e2"
+news_api_key = 
 
-TWILIO_SID = "a3084906649044337b74f359f8df481b"
-TWILIO_AUTH_TOKEN = "AC9077fa16f8b703c5b8d1f4aaeb5c7dae"
+TWILIO_SID = 
+TWILIO_AUTH_TOKEN = 
 
 class StockCheck():
     def __init__(self, symbol):
